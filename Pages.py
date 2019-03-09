398eabf324dd68733ca0d3bc75cbe304968b8a9f
Yoko Zoko <kawaiieraser@protonmail.com>
2019-03-08 16:37:34 -0800

    more

--- Pages.py
+++ Pages.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import Select
 from http.client import HTTPException
 from selenium.webdriver import ActionChains
 import time
+
 class accom_pages:
     def __init__(self, url):
         self.url = url
@@ -36,8 +37,22 @@ class accom_pages:
         # Use for Instruction pages
         driver.find_element_by_xpath('//*[@id="form"]/div/button').click()
 
+    def personalPage(self, driver):
+        ##driver.find_element_by_xpath
+        pass
+
     def preferences(self, driver):
         en = driver.find_element_by_xpath('//*[@id="mMyRange1"]')
         move = ActionChains(driver)
         move.click_and_hold(en).move_by_offset(0.5, 0).click()
+
+    def matchOpponent(self, driver):
+        en = driver.find_element_by_xpath('//*[@id="mMyRange1"]')
+        move = ActionChains(driver)
+        move.click_and_hold(en).move_by_offset(0.5, 0).click()
+
+    def waitPage(self, driver):
+        page = 'otree/WaitPage.html'
+        while driver.title == page:
+            time.sleep(5)
 
